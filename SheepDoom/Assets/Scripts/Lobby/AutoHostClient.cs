f05894706d8f995d03ed64ea442a9f3c3f57fd53
mapleleafrog <jiahao452@gmail.com>
2021-03-01 20:14:12 +0800

    lobby making pt 2
    still not working but progress

---
+++ SheepDoom/Assets/Scripts/Lobby/AutoHostClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class AutoHostClient : MonoBehaviour
+{
+    [SerializeField] NetworkManager networkManager;
+
+    /*
+    void Start()
+    {
+        if (!Application.isBatchMode)
+        {
+            Debug.Log("Client Connected");
+            networkManager.StartClient();
+        }
+
+        else
+        {
+            Debug.Log("Server Starting");
+        }
+    }
+    */
+    public void JoinLocal() 
+    {
+        Debug.Log("Start Game Pressed");
+        networkManager.networkAddress = "localhost";
+        networkManager.StartHost();
+        Debug.Log("Server started");
+    }
+}
