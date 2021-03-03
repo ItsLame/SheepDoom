decec197eee4e5e848c24b27f466db39d444ba21
greeengiraffe <68245980+greeengiraffe@users.noreply.github.com>
2021-03-03 16:22:02 +0800

    Added joystick (script not working yet)
    Added joystick (script not working yet)

---
+++ SheepDoom/Assets/Scripts/Game/CharacterMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+    private int idle;
+    private Rigidbody myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(CrossPlatformInputManager.GetAxis("Horizontal") > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            if (CrossPlatformInputManager.GetAxis("Vertical") > 0)
+            {
+                Vector3 moveVec = new Vector3(1, 0, 1) * speed;
+                myRigidBody.velocity = moveVec;
+            }else if (CrossPlatformInputManager.GetAxis("Vertical") < 0)
+            {
+                Vector3 moveVec = new Vector3(1, 0, -1) * speed;
+                myRigidBody.velocity = moveVec;
+            }
+            idle = 1;
+        } else if (CrossPlatformInputManager.GetAxis("Horizontal") < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            if(CrossPlatformInputManager.GetAxis("Vertical") > 0)
+            {
+                Vector3 moveVec = new Vector3(-1, 0, 1) * speed;
+                myRigidBody.velocity = moveVec;
+            }else if (CrossPlatformInputManager.GetAxis("Vertical") < 0)
+            {
+                Vector3 moveVec = new Vector3(-1, 0, -1) * speed;
+                myRigidBody.velocity = moveVec;
+            }
+            idle = 1;
+        }
+        else
+        {
+            if (idle == 1)
+            {
+                Vector3 moveVec = new Vector3(0, 0, 0);
+                myRigidBody.velocity = moveVec;
+                idle = 0;
+            }
+        }
+    }
+}
