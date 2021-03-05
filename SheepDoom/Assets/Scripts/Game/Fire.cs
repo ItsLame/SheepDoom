e10db4ec4f06d1c5109c5f084d6e0f7038059481
MemoryDespair <77581856+MemoryDespair@users.noreply.github.com>
2021-03-05 22:37:54 +0800

    Simple Projectile attack(WIP)

---
+++ SheepDoom/Assets/Scripts/Game/Fire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+ {
+     public GameObject Projectile;   
+     public Transform SpawnPoint; 
+ 
+     private void Update()
+     {
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+             Instantiate(Projectile, SpawnPoint.position, SpawnPoint.rotation);
+         }
+     }
+ }
---
+++ SheepDoom/Assets/Scripts/Game/ProjectileSpeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ProjectileSpeed : MonoBehaviour
+{
+
+    public float m_Speed = 10f;   // default speed of projectile
+    public float m_Lifespan = 3f; // Lifespan per second
+
+    private Rigidbody m_Rigidbody;
+
+    void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        m_Rigidbody.AddForce(transform.forward * m_Speed);
+        Destroy(gameObject, m_Lifespan);
+    }
+}
