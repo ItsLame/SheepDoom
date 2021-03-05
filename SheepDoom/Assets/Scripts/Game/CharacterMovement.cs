d5e06e4a61282fbadd1084f9fedd0f46469fe26b
greeengiraffe <68245980+greeengiraffe@users.noreply.github.com>
2021-03-06 00:29:04 +0800

    fixed player rotation keep resetting to 0 when stop joystick
    fixed player rotation keep resetting to 0 when stop joystick

--- SheepDoom/Assets/Scripts/Game/CharacterMovement.cs
+++ SheepDoom/Assets/Scripts/Game/CharacterMovement.cs
@@ -12,55 +12,19 @@ public class CharacterMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-  //      Debug.Log("Start joystick");
         myRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-   //     Debug.Log("fixedupdate joystick");
         Vector3 moveMe = new Vector3(CrossPlatformInputManager.GetAxis("Vertical"), 0.0f,
                                      -CrossPlatformInputManager.GetAxis("Horizontal")) * speed;
 
-        myRigidBody.rotation = Quaternion.LookRotation(moveMe);
-        myRigidBody.velocity = moveMe;
-
-        /*if(CrossPlatformInputManager.GetAxis("Horizontal") > 0)
+        if ((moveMe.x != 0) || (moveMe.z != 0))
         {
-            transform.localScale = new Vector3(1, 1, 1);
-            if (CrossPlatformInputManager.GetAxis("Vertical") > 0)
-            {
-                Vector3 moveVec = new Vector3(1, 0, 1) * speed;
-                myRigidBody.velocity = moveVec;
-            }else if (CrossPlatformInputManager.GetAxis("Vertical") < 0)
-            {
-                Vector3 moveVec = new Vector3(1, 0, -1) * speed;
-                myRigidBody.velocity = moveVec;
-            }
-            idle = 1;
-        } else if (CrossPlatformInputManager.GetAxis("Horizontal") < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-            if(CrossPlatformInputManager.GetAxis("Vertical") > 0)
-            {
-                Vector3 moveVec = new Vector3(-1, 0, 1) * speed;
-                myRigidBody.velocity = moveVec;
-            }else if (CrossPlatformInputManager.GetAxis("Vertical") < 0)
-            {
-                Vector3 moveVec = new Vector3(-1, 0, -1) * speed;
-                myRigidBody.velocity = moveVec;
-            }
-            idle = 1;
+            myRigidBody.rotation = Quaternion.LookRotation(moveMe);
         }
-        else
-        {
-            if (idle == 1)
-            {
-                Vector3 moveVec = new Vector3(0, 0, 0);
-                myRigidBody.velocity = moveVec;
-                idle = 0;
-            }
-        }*/
+        myRigidBody.velocity = moveMe;
     }
 }
