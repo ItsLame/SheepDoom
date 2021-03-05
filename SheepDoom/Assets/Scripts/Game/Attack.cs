cb27341a94b76db16eddfcc50e0d16bcbe91fc2c
greeengiraffe <68245980+greeengiraffe@users.noreply.github.com>
2021-03-06 00:18:51 +0800

    tie attack script to attack button
    tie attack script to attack button

--- SheepDoom/Assets/Scripts/Game/Attack.cs
+++ SheepDoom/Assets/Scripts/Game/Attack.cs
@@ -5,46 +5,26 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     private bool attacking = false;
-    //private float attackTimer = 0;
-    //private float attackCd = 0.3f;
-    //public Collider2D attacktrigger;
-    private Animation anim;
+    public GameObject Projectile;
+    public Transform SpawnPoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = gameObject.GetComponent<Animation>();
-        //attacktrigger.enabled = false;
+
     }
 
     public void AttackClick()
     {
         attacking = true;
-        //attackTimer = attackCd;
-        //attacktrigger.enabled = true;
-
     }
     // Update is called once per frame
     void Update()
     {
         if (attacking)
         {
-            Debug.Log("attack TRUE");
-            anim.Play("attack");
+            Instantiate(Projectile, SpawnPoint.position, SpawnPoint.rotation);
+            attacking = false;
         }
-        /*
-        if (attacking)
-        {
-            if (attackTimer > 0)
-            {
-                attackTimer -= Time.deltaTime;
-            }else
-            {
-                attacking = false;
-                //attacktrigger.enabled = false;
-            }
-        }
-        anim.Play("attack");
-        */
     }
 }
