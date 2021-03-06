0b3a44d12c5be3f08994094cf88754e63cdff457
MemoryDespair <77581856+MemoryDespair@users.noreply.github.com>
2021-03-06 20:05:04 +0800

    added basic healthbar and stats(wip)

---
+++ SheepDoom/Assets/Scripts/Game/Character/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+
+    private int currenthealth;
+
+    public event Action<float> OnHealthPctChanged = delegate { };
+
+    private void OnEnable()
+    {
+        currenthealth = maxHealth;
+
+    }
+    private void modifyinghealth(int amount)
+    {
+        currenthealth += amount;
+
+        float currenthealthPct = (float)currenthealth / (float)maxHealth;
+        OnHealthPctChanged(currenthealthPct);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            modifyinghealth(-10);
+        
+    }
+}
