587bb75d55131c4c214fc2a84044210572af1e1c
doomsheep1 <xuan_su_1997@hotmail.sg>
2021-03-08 18:23:04 +0800

    UI improvements

--- SheepDoom/Assets/Scripts/Login/AutoHostClient.cs
+++ SheepDoom/Assets/Scripts/Login/AutoHostClient.cs
@@ -8,8 +8,6 @@ using Mirror;
 public class AutoHostClient : MonoBehaviour
 {
     [SerializeField] NetworkManager networkManager;
-    public GameObject OptionsUI;
-    public GameObject CreditsUI;
     public InputField username;
     public InputField password;
 
@@ -34,24 +32,5 @@ public class AutoHostClient : MonoBehaviour
         networkManager.StartClient();
     }
 
-    //literally what they mean
-    public void OpenOptions()
-    {
-        OptionsUI.SetActive(true);
-    }
-
-    public void CloseOptions()
-    {
-        OptionsUI.SetActive(false);
-    }
-
-    public void OpenCredits()
-    {
-        CreditsUI.SetActive(true);
-    }
-
-    public void CloseCredits()
-    {
-        CreditsUI.SetActive(false);
-    }
+    public void Register() { }
 }
