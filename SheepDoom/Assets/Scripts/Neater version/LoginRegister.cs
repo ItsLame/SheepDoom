009a08162002680fadc650c6e73a2614dbf05145
doomsheep1 <xuan_su_1997@hotmail.sg>
2021-03-09 14:01:29 +0800

    client login

---
+++ SheepDoom/Assets/Scripts/Neater version/LoginRegister.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+namespace SheepDoom
+{
+    public class LoginRegister : MonoBehaviour
+    {
+        [SerializeField] NetworkManager networkManager;
+        public InputField username;
+        public InputField password;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (!Application.isBatchMode)
+            { //Headless build
+                Debug.Log($"=== Client Build ===");
+
+            }
+            else
+            {
+                Debug.Log($"=== Server Build ===");
+                networkManager.StartServer();
+            }
+        }
+
+        public void Login()
+        {
+            string user = username.text;
+            if(Client.ClientLogin(user))
+            {
+                networkManager.networkAddress = "localhost";
+                networkManager.StartClient(); // change to StartHost() if you don't want to build to test and just wanna use unity editor to test
+            }
+        }
+
+        public void Register() { }
+    }
+}
