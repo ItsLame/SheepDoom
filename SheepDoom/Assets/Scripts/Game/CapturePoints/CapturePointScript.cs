5851413415a73da60709687c53a70fb51cc8f2af
mapleleafrog <jiahao452@gmail.com>
2021-03-11 22:02:30 +0800

    map tower add
    x

---
+++ SheepDoom/Assets/Scripts/Game/CapturePoints/CapturePointScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CapturePointScript : MonoBehaviour
+{
+    public Text TotalTowerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player In Zone");
+        }
+    }
+}
