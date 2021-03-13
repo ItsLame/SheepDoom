243723a4231b1be8a3c6d1aa8f9de4097c593e13
doomsheep1 <xuan_su_1997@hotmail.sg>
2021-03-13 14:17:59 +0800

    stopped at lobby manager

--- SheepDoom/Assets/Scripts/Neater version/LobbyManager.cs
+++ SheepDoom/Assets/Scripts/Neater version/LobbyManager.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
 using Mirror;
 using System.Collections.Generic;
-
+using UnityEngine.UI;
+// The gameobject this script is attached to should only be spawned as a prefab on the server
 public class LobbyManager : NetworkBehaviour
 {
+    [SerializeField]
+    Button StartGameBtn;
+    [SerializeField]
+    Button SwitchTeamBtn;
 
+    private void StartGame()
+    {
+
+    }
+    private void SwitchTeam()
+    {
+
+    }
     #region Start & Stop Callbacks
 
     /// <summary>
