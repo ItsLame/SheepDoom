c2fdd4699aa6fce81cbbac43e5dc11517283ac9c
MemoryDespair <77581856+MemoryDespair@users.noreply.github.com>
2021-03-14 19:22:36 +0800

    added spawingsystem(creep movements wip)

--- SheepDoom/Assets/Scripts/Game/Character/PlayerHealth.cs
+++ SheepDoom/Assets/Scripts/Game/Character/PlayerHealth.cs
@@ -16,6 +16,7 @@ public class PlayerHealth : MonoBehaviour
     {
         currenthealth = maxHealth;
 
+
     }
     public void modifyinghealth(int amount)
     {
@@ -43,7 +44,14 @@ public class PlayerHealth : MonoBehaviour
         Rigidbody myRigidBody = GetComponent<Rigidbody>();
         Vector3 moveMe = new Vector3(0, 1, 0);
         myRigidBody.rotation = Quaternion.LookRotation(moveMe);
-
+        StartCoroutine(TimeBeforeDeath());
         Debug.Log("health: ded");
+    }
+
+    IEnumerator TimeBeforeDeath()
+    {
+        // should be sync with death animation
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
     }
 }
