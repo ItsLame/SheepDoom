85891992e3704f90d6d8eced5ed1bbf1dce6673f
MemoryDespair <77581856+MemoryDespair@users.noreply.github.com>
2021-03-15 16:16:03 +0800

    AddedBasicCreepWaypoints

---
+++ SheepDoom/Assets/Scripts/Creeps/TeamCoalitionLeftCreepwaymarksController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TeamCoalitionLeftCreepwaymarksController : MonoBehaviour
+{
+    public Transform[] points;
+    private int destPoint = 0;
+    private int maxWaypoint;
+    private NavMeshAgent agent;
+    public float CreepMovementSpeed = 2.0f;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+        agent.speed = CreepMovementSpeed;
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f && destPoint != maxWaypoint)
+            GotoNextPoint();
+        else
+        {
+            agent.autoBraking = true;
+            Debug.Log("Creep reached ");
+        }
+    }
+}
