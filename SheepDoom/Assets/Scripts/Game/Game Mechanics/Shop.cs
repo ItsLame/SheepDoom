d44bd8b006a033cec050d32863f6c8ac00d17e0b
mapleleafrog <jiahao452@gmail.com>
2021-03-15 16:43:35 +0800

    shop script

--- SheepDoom/Assets/Scripts/Game/Game Mechanics/Shop.cs
+++ SheepDoom/Assets/Scripts/Game/Game Mechanics/Shop.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,16 @@ public class Shop : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //look at player when in range
+    private void OnTriggerStay(Collider other)
+    {
+        //if (isLocalPlayer)
+        if (other.tag == "Player")
+        {
+            transform.LookAt(other.transform);
+        }
+
     }
 }
