5139213ffdebf5a7ca9e9b74bbd606c489db96b2
mapleleafrog <jiahao452@gmail.com>
2021-03-18 22:51:57 +0800

    prefab change

--- SheepDoom/Assets/Scripts/Game/Game Mechanics/ShopLookAt.cs
+++ SheepDoom/Assets/Scripts/Game/Game Mechanics/ShopLookAt.cs
@@ -18,27 +18,15 @@ public class ShopLookAt : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //if (isLocalPlayer)
+
         if (other.CompareTag("Player")) 
         {
             playerInView = true;
             playerWhoIsLookedAt = other.gameObject;
-     //       transform.LookAt(other.transform);
         }
 
     }
 
-    /*
-//look at player when in range
-private void OnTriggerStay(Collider other)
-{
-    //if (isLocalPlayer)
-    if (other.CompareTag("Player"))
-    {
-        transform.LookAt(other.transform);
-    }
-
-} */
 
     private void OnTriggerExit(Collider other)
     {
@@ -46,7 +34,6 @@ private void OnTriggerStay(Collider other)
         {
             playerInView = false;
             playerWhoIsLookedAt = null;
-            //       transform.LookAt(other.transform);
         }
     }
 
