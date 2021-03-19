1f68ffd6db5ee2dd36d7748781b741de82105461
greeengiraffe <68245980+greeengiraffe@users.noreply.github.com>
2021-03-19 16:45:40 +0800

    healing pad
    healing pad

---
+++ SheepDoom/Assets/Scripts/Game/CapturePoints/HealingPad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingPad : MonoBehaviour
+{
+    //Rate of healing
+    [SerializeField]
+    private float HealRate = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //for capture hp reduction when staying in area
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("inside healing pad");
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerHealth>().modifyinghealth(HealRate);
+            Debug.Log("healing: " + HealRate * Time.deltaTime);
+        }
+    }
+}
