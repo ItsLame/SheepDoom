3c07c133140372e1c7f9ca489fb15df9ceb34c31
ItsLame <37344470+ItsLame@users.noreply.github.com>
2021-03-21 23:44:19 +0800

    minimap player sprite follow player look at camera
    + camera inside player prefab
    + sprite inside player prefab
    + look at camera script
    + new layer tag(?)

---
+++ SheepDoom/Assets/Scripts/Game/Camera/PlayerOnMinimap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerOnMinimap : MonoBehaviour
+{
+    [SerializeField] private Camera PlayerMiniMapCamera;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(PlayerMiniMapCamera.transform);
+    }
+}
