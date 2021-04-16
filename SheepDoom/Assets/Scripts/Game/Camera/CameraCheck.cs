0f4dc0311f4ade2f8a66b70885aec56b6192bf7c
ItsLame <37344470+ItsLame@users.noreply.github.com>
2021-04-17 00:39:12 +0800

    fix cam null errors on server

---
+++ SheepDoom/Assets/Scripts/Game/Camera/CameraCheck.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CameraCheck : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isServer)
+            this.gameObject.SetActive(true);
+        else
+            this.gameObject.SetActive(false);
+    }
+}
