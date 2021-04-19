c00ed527105ddda5d8013419a9ac1c5801b69afa
ItsLame <37344470+ItsLame@users.noreply.github.com>
2021-04-19 18:34:20 +0800

    [structure] Objective abstract class pt.2
    + BOOL FINALLY WORKING T_T
    - pt.3 maybe or maybe not; depends if CaptureBaseScript will change to match CapturePointScript

--- SheepDoom/Assets/Scripts/Game/Class/Objective.cs
+++ SheepDoom/Assets/Scripts/Game/Class/Objective.cs
@@ -9,7 +9,6 @@ namespace SheepDoom
     public abstract class Objective : NetworkBehaviour
     {
         private GameObject scoreGameObject;
-        private GameObject captureGameObject;
         
         // hp: base/tower hp
         // inGameHP: to be used in game, gonna be the one fluctuating basically
@@ -20,8 +19,8 @@ namespace SheepDoom
         [SyncVar] private float inGameHP;
 
         //captured bools
-        private bool capturedByBlue;
-        private bool capturedByRed;
+        [SyncVar] protected bool capturedByBlue;
+        [SyncVar] protected bool capturedByRed;
 
         //logging number to check if base/tower is under capture or not
         private int numOfCapturers;
@@ -34,12 +33,6 @@ namespace SheepDoom
         {
             get{return scoreGameObject;}
             set{scoreGameObject = value;}
-        }
-        
-        protected GameObject P_captureGameObject
-        {
-            get{return captureGameObject;}
-            set{captureGameObject = value;}
         }
 
         protected float P_hp
@@ -66,13 +59,13 @@ namespace SheepDoom
             set{regenRate = value;}
         }
 
-        protected bool P_capturedByBlue
+        protected virtual bool P_capturedByBlue
         {
             get{return capturedByBlue;}
             set{capturedByRed = value;}
         }
 
-        protected bool P_capturedByRed
+        protected virtual bool P_capturedByRed
         {
             get{return capturedByRed;}
             set{capturedByRed = value;}
@@ -102,81 +95,36 @@ namespace SheepDoom
             OnHealthPctChangedTower(currenthealthPct);
         }
 
+        protected void SetTowerColor()
+        {
+            if (P_capturedByBlue && !P_capturedByRed)
+                GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+            else if (P_capturedByRed && !P_capturedByBlue)
+                GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        }
+
         protected virtual void Update()
         {
-            /*
-            // regen hp if base/tower is not under capture
-            if ((P_numOfCapturers == 0) && (P_inGameHP < P_hp))
-            {
-                ModifyingHealth(P_regenRate * Time.deltaTime);
-            }
-
-            // change color when captured by blue
-            if (P_capturedByBlue)
-            {
-                Debug.Log("CAPTURED BY BLUE");
-                var captureRenderer = P_captureGameObject.GetComponent<Renderer>();
-                captureRenderer.material.SetColor("_Color", Color.blue);
-            }
-            // else its red
-            else if (P_capturedByRed)
-            {
-                Debug.Log("CAPTURED BY RED");
-                var captureRenderer = P_captureGameObject.GetComponent<Renderer>();
-                captureRenderer.material.SetColor("_Color", Color.red);
-            }
-            */
+            // empty
         }
 
         // check for player enter
         protected virtual void OnTriggerEnter(Collider _collider)
         {
-            /*
+            
             if (_collider.tag == "Player")
             {
-                // get player's team ID
+                //get player's team ID
                 float tID = _collider.gameObject.GetComponent<PlayerAdmin>().getTeamIndex();
-                //bool isDed = _collider.gameObject.GetComponent<PlayerHealth>().isPlayerDead();
-
-                // if point belongs to red, it can be captured by blue players
-                if (P_capturedByRed && (tID == 1))
-                {
-                    numOfCapturers += 1;
-                }
-
-                // if point belongs to blue, it can be captured by red players
-                if (P_capturedByBlue && (tID == 2))
-                {
-                    numOfCapturers += 1;
-                }
+                bool isDed = _collider.gameObject.GetComponent<PlayerHealth>().isPlayerDead();
+                if (((P_capturedByRed && tID == 1) || (P_capturedByBlue && tID == 2)) && !isDed)
+                    P_numOfCapturers += 1;
             }
-            */
         }
 
         protected virtual void OnTriggerStay(Collider _collider)
         {
-            /*
-            if (_collider.CompareTag("Player"))
-            {
-                float tID = _collider.gameObject.GetComponent<PlayerAdmin>().getTeamIndex();
-                //get info of is player dead or alive
-                bool isDed = _collider.gameObject.GetComponent<PlayerHealth>().isPlayerDead();
-
-                OnStay(_collider, tID);
-
-                // if point belongs to red, it can be captured by blue
-                if (P_capturedByRed && (tID == 1) && !isDed)
-                {
-                    ModifyingHealth(-(P_captureRate * Time.deltaTime));
-                }
-
-                // if point belongs to blue, it can be captured by red
-                if (P_capturedByBlue && (tID == 2) && !isDed)
-                {
-                    ModifyingHealth(-(P_captureRate * Time.deltaTime));
-                }
-            }
-            */
+            
         }
 
         protected virtual void OnStay(Collider _collider, float tID)
@@ -188,25 +136,112 @@ namespace SheepDoom
         // check for player exit
         protected virtual void OnTriggerExit(Collider _collider)
         {
-            /*
             if (_collider.CompareTag("Player"))
             {
-                // get player's team ID
+                //get player's team ID
                 float tID = _collider.gameObject.GetComponent<PlayerAdmin>().getTeamIndex();
-
-                // if point belongs to red, it can be captured by blue players
-                if (P_capturedByRed && (tID == 1))
-                {
+                bool isDed = _collider.gameObject.GetComponent<PlayerHealth>().isPlayerDead();
+                if (((P_capturedByRed && tID == 1) || (P_capturedByBlue && tID == 2)) && !isDed)
                     P_numOfCapturers -= 1;
-                }
-
-                // if point belongs to blue, it can be captured by red players
-                if (P_capturedByBlue && (tID == 2))
-                {
-                    P_numOfCapturers -= 1;
-                }
             }
-            */
         }
     }
 }
+
+#region archive
+
+//under update()
+
+/*
+// regen hp if base/tower is not under capture
+if ((P_numOfCapturers == 0) && (P_inGameHP < P_hp))
+{
+    ModifyingHealth(P_regenRate * Time.deltaTime);
+}
+
+// change color when captured by blue
+if (P_capturedByBlue)
+{
+    Debug.Log("CAPTURED BY BLUE");
+    var captureRenderer = P_captureGameObject.GetComponent<Renderer>();
+    captureRenderer.material.SetColor("_Color", Color.blue);
+}
+// else its red
+else if (P_capturedByRed)
+{
+    Debug.Log("CAPTURED BY RED");
+    var captureRenderer = P_captureGameObject.GetComponent<Renderer>();
+    captureRenderer.material.SetColor("_Color", Color.red);
+}
+*/
+
+//under onenter()
+
+/*
+if (_collider.tag == "Player")
+{
+    // get player's team ID
+    float tID = _collider.gameObject.GetComponent<PlayerAdmin>().getTeamIndex();
+    //bool isDed = _collider.gameObject.GetComponent<PlayerHealth>().isPlayerDead();
+
+    // if point belongs to red, it can be captured by blue players
+    if (P_capturedByRed && (tID == 1))
+    {
+        numOfCapturers += 1;
+    }
+
+    // if point belongs to blue, it can be captured by red players
+    if (P_capturedByBlue && (tID == 2))
+    {
+        numOfCapturers += 1;
+    }
+}
+*/
+
+//under onstay()
+
+/*
+if (_collider.CompareTag("Player"))
+{
+    float tID = _collider.gameObject.GetComponent<PlayerAdmin>().getTeamIndex();
+    //get info of is player dead or alive
+    bool isDed = _collider.gameObject.GetComponent<PlayerHealth>().isPlayerDead();
+
+    OnStay(_collider, tID);
+
+    // if point belongs to red, it can be captured by blue
+    if (P_capturedByRed && (tID == 1) && !isDed)
+    {
+        ModifyingHealth(-(P_captureRate * Time.deltaTime));
+    }
+
+    // if point belongs to blue, it can be captured by red
+    if (P_capturedByBlue && (tID == 2) && !isDed)
+    {
+        ModifyingHealth(-(P_captureRate * Time.deltaTime));
+    }
+}
+*/
+
+//under onexit()
+
+/*
+if (_collider.CompareTag("Player"))
+{
+    // get player's team ID
+    float tID = _collider.gameObject.GetComponent<PlayerAdmin>().getTeamIndex();
+
+    // if point belongs to red, it can be captured by blue players
+    if (P_capturedByRed && (tID == 1))
+    {
+        P_numOfCapturers -= 1;
+    }
+
+    // if point belongs to blue, it can be captured by red players
+    if (P_capturedByBlue && (tID == 2))
+    {
+        P_numOfCapturers -= 1;
+    }
+}
+*/
+#endregion
