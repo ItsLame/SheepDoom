f57489312be6888fa1f43db662bbe7d1261882ed
mapleleafrog <jiahao452@gmail.com>
2021-04-21 00:10:23 +0800

    grim reaper ball (with attacks)
    chases player in trigger range
    attacks player when in distance

---
+++ SheepDoom/Assets/Scripts/Game/Creeps/Neutral/NeutralAOEAttackScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace SheepDoom
+{
+
+    public class NeutralAOEAttackScript : NetworkBehaviour
+    {
+        [Header("Rate of AOE Growth")]
+        public float scale_speed;
+
+        [Header("AOE duration")]
+        public float AOE_Duration;
+
+        [Header("Will AOE destroy on contact")]
+        public bool destroyOnContact;
+
+        [Header("How much damage it does")]
+        public float damage;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Destroy after set time
+            Destroy(gameObject, AOE_Duration);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //expand over time
+            this.gameObject.transform.localScale = this.gameObject.transform.localScale * scale_speed;
+        }
+
+        [Server]
+        void OnTriggerEnter(Collider col)
+        {
+            //if hit player
+            if (col.gameObject.CompareTag("Player"))
+            {
+
+                //reduce HP of hit target
+                col.gameObject.GetComponent<PlayerHealth>().modifyinghealth(-damage);
+
+                //increase killer's kill count if target is killed
+                if (col.gameObject.GetComponent<PlayerHealth>().getHealth() <= 0)
+                {
+                    col.gameObject.GetComponent<PlayerHealth>().SetPlayerDead();
+                }
+
+                if (destroyOnContact)
+                {
+                    Object.Destroy(this.gameObject);
+                }
+
+            }
+        }
+
+    }
+
+}
--- SheepDoom/Assets/Scripts/Game/Creeps/Neutral/NeutralAggroScript.cs
+++ SheepDoom/Assets/Scripts/Game/Creeps/Neutral/NeutralAggroScript.cs
@@ -2,54 +2,164 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NeutralAggroScript : MonoBehaviour
+namespace SheepDoom
 {
-    public GameObject Attacker;
-    public bool LockedOn;
-    public float moveSpd;
+    public class NeutralAggroScript : MonoBehaviour
+    {
+        [Header("Current Target")]
+        public GameObject Attacker;
+        public bool LockedOn;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        [Header("Object stats")]
+        public float moveSpd;
+        public float moveSpdInGame;
+        public float attackRange;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        [Header("Attacking object to be instantiated")]
+        public GameObject AuraDamageObject;
+
+        [Header("Attack Cooldown")]
+        public float attackCooldown;
+        public float attackCooldownInGame;
+
+        [Header("Attack Animation Duration(Stops movement for x time")]
+        public bool isAttacking;
+        public float attackDuration;
+        public float attackDurationInGame;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            Debug.Log("Player" + other.gameObject.name + " spotted");
-            Attacker = other.gameObject;
-            LockedOn = true;
+            //initialize attacking timers
+            attackCooldownInGame = attackCooldown;
+            attackDurationInGame = -1;
+            isAttacking = false;
+            moveSpdInGame = moveSpd;
+        }
 
+        // Update is called once per frame
+        void Update()
+        {
+            if (attackCooldownInGame >= 0)
+            {
+                attackCooldownInGame -= Time.deltaTime;
+            }
+
+            //dont move when attacking
+            if (attackDurationInGame >= 0 && isAttacking)
+            {
+                moveSpdInGame = 0;
+                attackDurationInGame -= Time.deltaTime;
+            }
+
+            //not attacking when timer up
+            else
+            {
+                //start moving again
+                moveSpdInGame = moveSpd;
+                isAttacking = false;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            //if no target, lock onto a new player in range
+            if (!LockedOn)
+            {
+                if (other.CompareTag("Player"))
+                {
+                    Debug.Log("Player" + other.gameObject.name + " spotted");
+                    Attacker = other.gameObject;
+                    LockedOn = true;
+
+                }
+            }
+
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            //1st priority: lock on to first player
+            if (other.CompareTag("Player") && LockedOn && !other.gameObject.GetComponent<PlayerHealth>().isPlayerDead())
+            {
+                if (other.gameObject != Attacker.gameObject) return;
+
+                Debug.Log("Moving towards " + Attacker.gameObject.name);
+
+                //chase if not in attack range
+                if (Vector3.Distance(Attacker.transform.position, transform.position) > attackRange)
+                {
+                    transform.LookAt(Attacker.transform);
+                    this.gameObject.transform.position += transform.forward * moveSpdInGame * Time.deltaTime;
+                }
+
+                //if in attack range
+                else
+                {
+                    //attack if cooldown up
+                    if (attackCooldownInGame <= 0)
+                    {
+                        attackDurationInGame = attackDuration;
+                        isAttacking = true;
+                        Debug.Log("Death ball attacking");
+                        Instantiate(AuraDamageObject, this.gameObject.transform.position, this.gameObject.transform.rotation);
+                        //reset attack timer
+                        attackCooldownInGame = attackCooldown;
+                    }
+                }
+            }
+
+            //2nd priority: if 1st player out of range, change attacker to any other in range
+            else if (!LockedOn)
+            {
+                if (other.CompareTag("Player") && !other.gameObject.GetComponent<PlayerHealth>().isPlayerDead())
+                {
+                    LockedOn = true;
+                    Attacker = other.gameObject;
+                    Debug.Log("Moving towards " + Attacker.gameObject.name);
+
+                    if (Vector3.Distance(Attacker.transform.position, transform.position) > attackRange)
+                    {
+                        transform.LookAt(Attacker.transform);
+                        this.gameObject.transform.position += transform.forward * moveSpdInGame * Time.deltaTime;
+
+                    }
+
+                    //if in attack range
+                    else
+                    {
+                        //attack if cooldown up
+                        if (attackCooldownInGame <= 0)
+                        {
+                            attackDurationInGame = attackDuration;
+                            Debug.Log("Death ball attacking");
+                            isAttacking = true;
+                            Instantiate(AuraDamageObject, this.gameObject.transform.position, this.gameObject.transform.rotation);
+                            //reset attack timer
+                            attackCooldownInGame = attackCooldown;
+                        }
+                    }
+                }
+
+
+            }
+
+
+
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player") && LockedOn)
+            {
+                Debug.Log("Stopped following " + Attacker.gameObject.name);
+                Attacker = null;
+                LockedOn = false;
+            }
         }
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player") && LockedOn)
-        {
-            if (other.gameObject != Attacker.gameObject) return;
-
-            Debug.Log("Moving towards " + Attacker.gameObject.name);
-            this.gameObject.transform.position += transform.forward * moveSpd * Time.deltaTime;
-            transform.LookAt(Attacker.transform);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player") && LockedOn)
-        {
-            Debug.Log("Stopped following " + Attacker.gameObject.name);
-            Attacker = null;
-            LockedOn = false;
-        }
-    }
 }
+
+
+
