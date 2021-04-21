cf2f74eda68a9981cf2987a73dbe06c90ff0f084
mapleleafrog <jiahao452@gmail.com>
2021-04-21 14:18:47 +0800

    map update + new regions at side + health item

---
+++ SheepDoom/Assets/Scripts/Game/Game Mechanics/OnTouchHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace SheepDoom
+{
+    public class OnTouchHealth : NetworkBehaviour
+    {
+        [Header("Amount of health to change on contact")]
+        public float healthChangeAmount;
+
+        [Header("If object will be destroyed on contact")]
+        public bool destroyOnContact;
+
+        //when collide with player
+        private void OnTriggerEnter(Collider other)
+        {
+            if (isServer)
+            {
+                if (other.gameObject.CompareTag("Player"))
+                {
+                    other.gameObject.GetComponent<PlayerHealth>().modifyinghealth(healthChangeAmount);
+                    if (destroyOnContact)
+                    {
+                        Destroy(gameObject);
+                    }
+
+                }
+            }
+
+        }
+    }
+}
+
