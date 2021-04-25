5af6111d40e5842113f4f78018440af4e22b3c84
doomsheep1 <xuan_su_1997@hotmail.sg>
2021-04-25 11:03:16 +0800

    healing pad adjusted for damaging enemies

--- SheepDoom/Assets/Scripts/Game/Game Mechanics/Mechanics/HealingPad.cs
+++ SheepDoom/Assets/Scripts/Game/Game Mechanics/Mechanics/HealingPad.cs
@@ -11,7 +11,7 @@ namespace SheepDoom
         [SerializeField]
         private float HealRate = 0.2f;
 
-        [Server]
+        /*[Server]
         private void OnTriggerStay(Collider other)
         {
             //Debug.Log("inside healing pad");
@@ -28,6 +28,43 @@ namespace SheepDoom
                     if (other.gameObject.layer == 9)
                         other.gameObject.GetComponent<PlayerHealth>().modifyinghealth(HealRate);
                 }  
+            }
+        }*/
+
+        [Server]
+        private void OnTriggerStay(Collider other)
+        {
+            if(other.CompareTag("Player"))
+            {
+                if(gameObject.CompareTag("TeamCoaHealPad"))
+                {
+                    if(other.gameObject.layer == 8 && !other.GetComponent<PlayerHealth>().GetHealthStatus()) // heal ally
+                        other.GetComponent<PlayerHealth>().modifyinghealth(HealRate);
+                    else if(other.gameObject.layer == 9)
+                    {
+                        other.GetComponent<PlayerHealth>().modifyinghealth(-(HealRate * 2)); // damage enemy
+                        if(other.GetComponent<PlayerHealth>().getHealth() <= 0)
+                            other.GetComponent<PlayerHealth>().SetPlayerDead();
+                    }   
+                }
+                else if (gameObject.CompareTag("TeamConHealPad"))
+                {
+                    if (other.gameObject.layer == 9 && !other.GetComponent<PlayerHealth>().GetHealthStatus())
+                        other.GetComponent<PlayerHealth>().modifyinghealth(HealRate);
+                    else if (other.gameObject.layer == 8)
+                    {
+                        other.GetComponent<PlayerHealth>().modifyinghealth(-(HealRate * 2));
+                        if (other.GetComponent<PlayerHealth>().getHealth() <= 0)
+                            other.GetComponent<PlayerHealth>().SetPlayerDead();
+                    }  
+                }
+            }
+            else if(other.CompareTag("BaseMinion"))
+            {
+                if (gameObject.CompareTag("TeamCoaHealPad") && other.gameObject.layer == 9) // damage enemy minion
+                    other.GetComponent<LeftMinionBehaviour>().TakeDamage(-(HealRate * 2));
+                else if (gameObject.CompareTag("TeamConHealPad") && other.gameObject.layer == 8)
+                    other.GetComponent<LeftMinionBehaviour>().TakeDamage(-(HealRate * 2));
             }
         }
     }
