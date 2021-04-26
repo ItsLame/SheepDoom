e5d6a9db829355cd44565ec7c427a12e61751e15
ItsLame <37344470+ItsLame@users.noreply.github.com>
2021-04-27 00:33:31 +0800

    [fix] on host, disable server camera

--- SheepDoom/Assets/Scripts/Game/Camera/CameraCheck.cs
+++ SheepDoom/Assets/Scripts/Game/Camera/CameraCheck.cs
@@ -8,7 +8,9 @@ public class CameraCheck : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(isServer)
+        if(isServer && isClient)
+            this.gameObject.SetActive(false);
+        else if(isServer)
             this.gameObject.SetActive(true);
         else
             this.gameObject.SetActive(false);
