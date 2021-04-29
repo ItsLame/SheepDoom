0ba302f245266a801000b87a7936cebc2e050aea
ItsLame <37344470+ItsLame@users.noreply.github.com>
2021-04-29 20:42:10 +0800

    [fix] fixed shop... i hope?
    + im blind

--- SheepDoom/Assets/Scripts/Game/Character/Admin Related/PlayerAdmin.cs
+++ SheepDoom/Assets/Scripts/Game/Character/Admin Related/PlayerAdmin.cs
@@ -27,18 +27,6 @@ namespace SheepDoom
 
         private GameObject shop;
 
-        private void Start()
-        {
-            if(isClient)
-                CmdSetTeamIndex(PlayerObj.instance.GetTeamIndex());
-        }
-
-        [Command]
-        private void CmdSetTeamIndex(int _teamIndex)
-        {
-            setTeamIndex(_teamIndex);
-        }
-
         //accessor method for team index
         public float getTeamIndex()
         {
@@ -86,6 +74,12 @@ namespace SheepDoom
                 PlayerDeathsText.text = newValue.ToString();
         }
 
+        [Command]
+        private void CmdSetTeamIndex(int _teamIndex)
+        {
+            setTeamIndex(_teamIndex);
+        }
+
         public override void OnStartClient()
         {
             if (!hasAuthority) return;
@@ -95,12 +89,13 @@ namespace SheepDoom
             PlayerKillsText.text = PlayerKills.ToString();
             PlayerDeathsText.text = PlayerDeaths.ToString();
             TowerCapturesText.text = TowerCaptures.ToString();
-
-            if (gameObject.GetComponent<PlayerAdmin>().getTeamIndex() == 1)
-                shop = FindMe.instance.P_BlueShop;
-            else if (gameObject.GetComponent<PlayerAdmin>().getTeamIndex() == 2)
-                shop = FindMe.instance.P_RedShop;
-            shop.GetComponent<Shop>().SetPlayer(gameObject);
+            
+            int playerTeamID = PlayerObj.instance.GetTeamIndex();
+            CmdSetTeamIndex(playerTeamID);
+            if (playerTeamID == 1)
+                FindMe.instance.P_BlueShop.GetComponent<Shop>().P_shopPlayer = gameObject;
+            else if (playerTeamID == 2)
+                FindMe.instance.P_RedShop.GetComponent<Shop>().P_shopPlayer = gameObject;
         }
 
         public override void OnStartServer()
---
+++ SheepDoom/Assets/Scripts/Game/Game Mechanics/BlueShop.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SheepDoom
+{
+    public class BlueShop : Shop
+    {
+        [SerializeField] private GameObject ShopPlayer;
+        [SerializeField] private float PlayerGold;
+        [SerializeField] private int ShopTeamIndex = 1;
+
+        [Space(15)]
+        [SerializeField] private float SpecialCost;
+        [SerializeField] private float UltiCost;
+
+        [Space(15)]
+        //shop's UI
+        [SerializeField] public GameObject ShopMenuUI;
+
+        [Space(15)]
+        //shops buttons
+        [SerializeField] private GameObject SpecialButton1;
+        [SerializeField] private GameObject SpecialButton2;
+        [SerializeField] private GameObject UltiButton1;
+        [SerializeField] private GameObject UltiButton2;
+
+        [Space(15)]
+        //game's control UI
+        [SerializeField] private GameObject GameUI;
+        //player special & ulti button
+        [SerializeField] private GameObject PlayerSpecialButton;
+        [SerializeField] private GameObject PlayerUltiButton;
+
+        public override GameObject P_shopPlayer { get => ShopPlayer; set => ShopPlayer = value; }
+        //public override float P_playerGold { get => PlayerGold; set => PlayerGold = value; }
+
+        protected override void InitShop()
+        {
+            P_playerGold = PlayerGold;
+            P_shopTeamIndex = ShopTeamIndex;
+            P_specialCost = SpecialCost;
+            P_ultiCost = UltiCost;
+            P_shopMenuUI = ShopMenuUI;
+            P_specialButton1 = SpecialButton1;
+            P_specialButton2 = SpecialButton2;
+            P_ultiButton1 = UltiButton1;
+            P_ultiButton2 = UltiButton2;
+            P_gameUI = GameUI;
+            P_playerSpecialButton = PlayerSpecialButton;
+            P_playerUltiButton = PlayerUltiButton;
+        }
+    }
+}
+
+#region archive
+/*
+//[Space(15)]
+//control checks for if player purchased special / ulti
+//[SerializeField] private bool HasPurchasedSpecial = false;
+//[SerializeField] private bool HasPurchasedUlti = false;
+
+public override int P_shopTeamIndex { get => ShopTeamIndex; set => P_shopTeamIndex = value; }
+public override float P_specialCost { get => SpecialCost; set => SpecialCost = value; }
+public override float P_ultiCost { get => UltiCost; set => UltiCost = value; }
+public override GameObject P_shopMenuUI { get => ShopMenuUI; set => ShopMenuUI = value; }
+public override GameObject P_specialButton1 { get => SpecialButton1; set => SpecialButton1 = value; }
+public override GameObject P_specialButton2 { get => SpecialButton2; set => SpecialButton2 = value; }
+public override GameObject P_ultiButton1 { get => UltiButton1; set => UltiButton1 = value; }
+public override GameObject P_ultiButton2 { get => UltiButton2; set => UltiButton2 = value; }
+public override GameObject P_gameUI { get => GameUI; set => GameUI = value; }
+public override GameObject P_playerSpecialButton { get => PlayerSpecialButton; set => PlayerSpecialButton = value; }
+public override GameObject P_playerUltiButton { get => PlayerUltiButton; set => PlayerUltiButton = value; }
+*/
+#endregion
