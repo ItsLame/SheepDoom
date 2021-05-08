309cca063aea6a9a884e3714ef523953a8e6a513
mapleleafrog <jiahao452@gmail.com>
2021-05-08 20:04:07 +0800

    [moving platform] not sync yet

---
+++ SheepDoom/Assets/StartAnimatingScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class StartAnimatingScript : NetworkBehaviour
+{
+    public override void OnStartServer()
+    {
+        gameObject.GetComponent<NetworkAnimator>().SetTrigger("StartMoving");
+    }
+}
