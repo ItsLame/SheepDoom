c9e9f4f82854513edb30674f43bf70582af47363
mapleleafrog <jiahao452@gmail.com>
2021-05-11 21:58:10 +0800

    [Lobby UI] revamp
    - added play, credits and options
    - transition between login and lobby
    - style change
    - feedbak if have pls

---
+++ SheepDoom/Assets/Scripts/Lobby/UI/MainMenuUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public Animator transition;
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+
+    public void FadeOut()
+    {
+        transition.SetTrigger("FadeOut");
+    }
+}
