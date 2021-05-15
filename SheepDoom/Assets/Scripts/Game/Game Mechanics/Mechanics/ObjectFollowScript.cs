c217822cebc42e80813f763bc859001ece587d19
mapleleafrog <jiahao452@gmail.com>
2021-05-15 19:33:10 +0800

    [ASTAROTH] death + shield fix n update

--- SheepDoom/Assets/Scripts/Game/Game Mechanics/Mechanics/ObjectFollowScript.cs
+++ SheepDoom/Assets/Scripts/Game/Game Mechanics/Mechanics/ObjectFollowScript.cs
@@ -7,47 +7,11 @@ namespace SheepDoom
 {
     public class ObjectFollowScript : NetworkBehaviour
     {
-        /*
-        //setting owner
-        public GameObject owner;
-        public Vector3 ownerTransform;
-
-        //duration
-        [SerializeField] private float duration, durationInGame;
-
-        public void setOwner(GameObject objectOwner)
-        {
-            owner = objectOwner;
-        }
-
-        private void Start()
-        {
-            durationInGame = duration;
-            ownerTransform = owner.gameObject.transform.position;
-        }
-        // Update is called once per frame
-        void Update()
-        {
-            // object position is based of owner position + set distance
-            this.gameObject.transform.position = owner.gameObject.transform.position + owner.transform.TransformDirection(0, 0, 15);
-            this.gameObject.transform.LookAt(owner.gameObject.transform);
-
-
-            //timers
-            durationInGame -= Time.deltaTime;
-
-            if (durationInGame <= 0)
-            {
-                Destroy(this.gameObject);
-            }
-        }*/
-
-
         //duration
         [SerializeField] private float duration, durationInGame;
 
         private Vector3 relativePosition;
-        private Vector3 wantedPosition = new Vector3(0, 0, 1.5f);
+        private Vector3 wantedPosition = new Vector3(0, 0, 10f);
         [SyncVar] public GameObject owner;
         public Transform ownerTransform;
         [SyncVar] public float teamID;
@@ -78,3 +42,38 @@ namespace SheepDoom
     }
 
 }
+
+/*
+//setting owner
+public GameObject owner;
+public Vector3 ownerTransform;
+
+//duration
+[SerializeField] private float duration, durationInGame;
+
+public void setOwner(GameObject objectOwner)
+{
+    owner = objectOwner;
+}
+
+private void Start()
+{
+    durationInGame = duration;
+    ownerTransform = owner.gameObject.transform.position;
+}
+// Update is called once per frame
+void Update()
+{
+    // object position is based of owner position + set distance
+    this.gameObject.transform.position = owner.gameObject.transform.position + owner.transform.TransformDirection(0, 0, 15);
+    this.gameObject.transform.LookAt(owner.gameObject.transform);
+
+
+    //timers
+    durationInGame -= Time.deltaTime;
+
+    if (durationInGame <= 0)
+    {
+        Destroy(this.gameObject);
+    }
+}*/
