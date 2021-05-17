6a32b04d5c94fa30f245681e7d87927f77ef595c
ItsLame <37344470+ItsLame@users.noreply.github.com>
2021-05-17 23:41:05 +0800

    Revert "[fix] missing network manager when disconnect"
    This reverts commit 623fd47432c7ce4033e998f4019ba53013ae5f25.

--- SheepDoom/Assets/Scripts/Lobby/LoginRegister.cs
+++ SheepDoom/Assets/Scripts/Lobby/LoginRegister.cs
@@ -8,17 +8,15 @@ namespace SheepDoom
 {
     public class LoginRegister : MonoBehaviour
     {
-        [SerializeField] private NetworkManager networkManager;
+        [SerializeField] NetworkManager networkManager;
         [SerializeField] private InputField username;
+        [SerializeField] private InputField password;
 
         public static string SessionTicket;
 
         // Start is called before the first frame update
         void Start()
         {
-            if(!networkManager)
-                networkManager = SDNetworkManager.singleton.gameObject.GetComponent<NetworkManager>();
-
             if (!Application.isBatchMode) //Headless build
                 Debug.Log($"=== Client Build ===");
             else
